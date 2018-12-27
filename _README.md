b02910511d8d6de9037eb4a1715e70d9ae3eb2d3
shaomingquan <shaomingquan@meituan.com>
2018-12-28 00:34:22 +0800

    build not draft together

---
+++ _README.md
@@ -0,0 +1,9 @@
+### hi
+
+[🍭 ***for more interesting things***](http://shaomingquan.github.io/laboratory/)
+
+this is my blog
+
+write by [mdmd](https://github.com/shaomingquan/mdmd)
+
+wechat 1218525419
---
+++ build.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+const getLinkByFileName = name => `https://github.com/shaomingquan/articles/blob/master/src/${name}.md`
+
+const makeBlogItem = name => `- [${name}](${getLinkByFileName(name)})
+`
+
+const _files = fs.readdirSync('./src')
+
+const header = fs.readFileSync('./_README.md').toString()
+
+const divider = `
+
+---------------------------articles----------------------------
+
+`
+
+let body = ''
+
+const files = _files
+.filter(filename => filename.indexOf('.md') > -1 && !filename.startsWith('_draft_'))
+.map(filename => filename.substring(0, filename.length - 3))
+
+files.forEach(filename => (body += makeBlogItem(filename)))
+
+fs.writeFileSync('./README.md', header + divider + body)
